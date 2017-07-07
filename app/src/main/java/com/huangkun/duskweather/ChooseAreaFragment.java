c2069f718e2525424953b7602149ade4fb933100
huangkun <1780140808@qq.com>
2017-07-07 21:41:07 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/huangkun/duskweather/ChooseAreaFragment.java
+++ app/src/main/java/com/huangkun/duskweather/ChooseAreaFragment.java
@@ -18,6 +18,7 @@ import android.widget.Toast;
 import com.huangkun.duskweather.db.City;
 import com.huangkun.duskweather.db.County;
 import com.huangkun.duskweather.db.Province;
+import com.huangkun.duskweather.gson.Weather;
 import com.huangkun.duskweather.util.HttpUtil;
 import com.huangkun.duskweather.util.Utility;
 
@@ -91,10 +92,18 @@ public class ChooseAreaFragment extends Fragment {
                     queryCounties();
                 }else if (currentLevel==LEVEL_COUNTY){
                     String weatherId=countyList.get(position).getWeatherId();
-                    Intent intent=new Intent(getActivity(),WeatherActivity.class);
-                    intent.putExtra("weather_id",weatherId);
-                    startActivity(intent);
-                    getActivity().finish();
+                    if(getActivity()instanceof MainActivity){
+                        Intent intent=new Intent(getActivity(),WeatherActivity.class);
+                        intent.putExtra("weather_id",weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    }else if (getActivity()instanceof WeatherActivity){
+                        WeatherActivity activity=(WeatherActivity)getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefresh.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+                    }
+
                 }
             }
         });
